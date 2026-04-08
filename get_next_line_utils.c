efac3aa09fb25e5bf660de81934dbdd37b1d94a1
betferna <betferna@student.codam.nl>
2026-04-09 00:33:24 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,148 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42seoul.kr>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/02 15:42:40 by betferna          #+#    #+#             */
-/*   Updated: 2026/04/06 11:55:02 by user             ###   ########seoul.kr  */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*c;
-	size_t			i;
+# include <fcntl.h>
+# include <limits.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-	c = (unsigned char *)s;
-	i = 0;
-	while (i < n)
-	{
-		c[i] = '\0';
-		i++;
-	}
-}
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-void	*ft_calloc(size_t num, size_t size)
-{
-	void	*p;
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *storage, char *buffer);
+char	*left_line(char *storage);
+char	*find_newline(char *st);
+char	*free_storage(char *storage);
+int		newline(char *st);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_line(char *buffer, char *storage, int buffer_size);
 
-	if (num != 0 && size > SIZE_MAX / num)
-		return (NULL);
-	p = malloc(num * size);
-	if (!p)
-		return (NULL);
-	ft_bzero(p, (num * size));
-	return (p);
-}
-
-
-size_t	ft_strlen(const char *str)
-{
-	int	x;
-
-	x = 0;
-	while (str[x] != '\0')
-	{
-		x++;
-	}
-	return (x);
-}
-
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		len;
-	char	*concat;
-	int		i;
-	int		j;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
-	concat = (char *)malloc(sizeof(char) * (len + 1));
-	if (!concat)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		concat[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		concat[i + j] = s2[j];
-		j++;
-	}
-	concat[i + j] = '\0';
-	return (concat);
-}
-
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t				i;
-	unsigned char		*d;
-	const unsigned char	*s;
-
-	s = (const unsigned char *)src;
-	d = (unsigned char *)dest;
-	i = 0;
-	if (!dest && !src)
-		return (NULL);
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	return (d);
-}
-
-
-char	*ft_strdup(const char *s)
-{
-	char	*str;
-	int		len;
-
-	len = ft_strlen(s) + 1;
-	str = (char *)malloc(sizeof(char) * len);
-	if (str == NULL)
-		return (NULL);
-	ft_memcpy(str, s, len);
-	return (str);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	while (*str)
-	{
-		if (*str == (char)c)
-			return ((char *)str);
-		str++;
-	}
-	if ((char)c == '\0')
-		return ((char *)str);
-	return (NULL);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*p;
-
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if (len > (ft_strlen(s)) - (size_t)start)
-		len = ft_strlen(s) - (size_t)start;
-	p = (char *)malloc(len + 1);
-	if (!p)
-		return (NULL);
-	p = ft_memcpy(p, s + start, len);
-	p[len] = '\0';
-	return (p);
-}
+#endif
