a45e2f13f193c4b995ea669078460dd55dee49bb
betferna <betferna@student.codam.nl>
2026-04-09 00:34:26 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,39 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42seoul.kr>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/02 14:23:10 by betferna          #+#    #+#             */
-/*   Updated: 2026/04/06 11:55:51 by user             ###   ########seoul.kr  */
-/*                                                                            */
-/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdint.h>
-#include <limits.h>
-#include <fcntl.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t num, size_t size);
+char	*ft_read(int fd, char *storage, char *buffer);
+char	*left_line(char *storage);
+char	*find_newline(char *st);
+char	*free_storage(char *storage);
+int		newline(char *st);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
-char 	*get_line(char *buffer, char *storage, int buffer_size);
+char	*get_line(char *buffer, char *storage, int buffer_size);
 
 #endif
