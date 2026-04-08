61cd57bf9b9abd6732cf721ab0dd641820c10075
betferna <betferna@student.codam.nl>
2026-04-09 00:33:57 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,30 +1,99 @@
+#include "get_next_line.h"
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	int		i;
 
-# include <fcntl.h>
-# include <limits.h>
-# include <stdint.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > (ft_strlen(s)) - (size_t)start)
+		len = ft_strlen(s) - (size_t)start;
+	p = (char *)malloc(len + 1);
+	if (!p)
+		return (NULL);
+	while (i < len)
+	{
+		p[i] = s[start + i];
+		i++;
+	}
+	p[len] = '\0';
+	return (p);
+}
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
+size_t	ft_strlen(const char *str)
+{
+	int	x;
 
-char	*get_next_line(int fd);
-char	*ft_read(int fd, char *storage, char *buffer);
-char	*left_line(char *storage);
-char	*find_newline(char *st);
-char	*free_storage(char *storage);
-int		newline(char *st);
-size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strchr(const char *str, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*get_line(char *buffer, char *storage, int buffer_size);
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
 
-#endif
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len;
+	char	*concat;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	concat = (char *)malloc(sizeof(char) * (len + 1));
+	if (!concat)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		concat[i + j] = s2[j];
+		j++;
+	}
+	concat[i + j] = '\0';
+	return (concat);
+}
+
+int	newline(char *st)
+{
+	int	i;
+
+	i = 0;
+	while (st[i])
+	{
+		if (st[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(s) + 1;
+	str = (char *)malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
