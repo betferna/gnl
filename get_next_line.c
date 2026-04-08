0a17f8ebb114e930782a7adeffd734aa3f79be64
betferna <betferna@student.codam.nl>
2026-04-09 00:36:08 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,132 +1,112 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42seoul.kr>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/02 13:01:45 by betferna          #+#    #+#             */
-/*   Updated: 2026/04/06 11:57:28 by user             ###   ########seoul.kr  */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-char *read_file(int fd, char *storage)
+char	*get_next_line(int fd)
 {
-    char    *buffer;
-    char    *temp;
-    int     bytes;
+	static char	*storage;
+	char		*buffer;
+	char		*line;
 
-    buffer = calloc((BUFFER_SIZE + 1), sizeof(char));
-    if (!buffer)
-        return (NULL);
-    bytes = 1;
-    while (bytes != 0)
-    {
-        bytes = read(fd, buffer, BUFFER_SIZE);
-        if (bytes == -1)
-        {
-            free(buffer);
-            free(storage);
-            return (NULL);
-        }
-        buffer[bytes] = '\0';
-        if (!storage)
-            storage = ft_strdup("");
-        temp = ft_strjoin(storage, buffer);
-        free(storage);
-        storage = temp;
-        if (ft_strchr(buffer, '\n'))
-            break;
-    }
-    free(buffer);
-    return (storage);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc((sizeof(char) * BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	if (!storage)
+		storage = ft_strdup("");
+	storage = ft_read(fd, storage, buffer);
+	if (!storage)
+		return (NULL);
+	line = find_newline(storage);
+	storage = left_line(storage);
+	return (line);
 }
 
-char *get_next_line(int fd)
+char	*ft_read(int fd, char *storage, char *buffer)
 {
-    static char *storage;
-    char        *line;
-    char        *temp;
-    int         i;
+	int		bytes;
+	char	*temp;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    storage = read_file(fd, storage);
-    if (!storage || *storage == '\0')
-    {
-        free(storage);
-        storage = NULL;
-        return (NULL);
-    }
-    i = 0;
-    while (storage[i] && storage[i] != '\n')
-        i++;
-    line = ft_substr(storage, 0, i + (storage[i] == '\n'));
-    temp = ft_strdup(storage + i + (storage[i] == '\n'));
-    free(storage);
-    storage = temp;
-    return (line);
+	bytes = 0;
+	while (!newline(storage))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == 0)
+			break ;
+		else if (bytes == -1 || !storage)
+			storage = free_storage(storage);
+		buffer[bytes] = '\0';
+		temp = storage;
+		storage = ft_strjoin(temp, buffer);
+		free(temp);
+	}
+	free(buffer);
+	if (!storage || *storage == '\0')
+		storage = free_storage(storage);
+	return (storage);
 }
 
-int main(void)
+char	*free_storage(char *storage)
 {
-    int     fd;
-    char    *line;
-
-    fd = open("archivo.txt", O_RDONLY);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line); 
-	free(line);
-    }
-    close(fd);
-    return (0);
+	if (storage)
+	{
+		free(storage);
+		storage = NULL;
+	}
+	return (NULL);
 }
-// void free_storage(char *storage)
-// {
-//     ft_bzero(storage, ft_strlen(storage));
-//     free(storage);
-// }
 
-// char *get_line(int fd, char *buffer, char *storage, int size)
-// {
-//     int 	bytes;
-//     bytes = 1;
-// 	buffer = calloc((BUFFER_SIZE + 1), (sizeof(char)));
-//     while ( bytes != 0)
-//     {
-//         bytes = read(fd, buffer, BUFFER_SIZE);
-//         if (bytes == -1) 
-// 	    {
-//             free(buffer);
-//             free(storage);
-//             return NULL;
-//         }
-//         buffer[bytes] = '\0';
-//         temp = ft_strjoin(storage, buffer);
-//         free(storage);
-//         storage = temp;
-//     }
-//     free(buffer);
-//     if (!storage) retorn NULL;
-//     if (!(ft_strchr(storage, '\n')))
-//     {
-//         get_line(buffer, storage, BUFFER_SIZE*2);
-//     }
-//     return (storage);
-// }
+char	*find_newline(char *st)
+{
+	char	*line;
+	int		i;
+	int		len;
 
-// char *get_next_line(int fd)
+	len = ft_strlen(st) + 2;
+	if (!st)
+		return (NULL);
+	line = malloc(sizeof(char) * len);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (st[i] && st[i] != '\n')
+	{
+		line[i] = st[i];
+		i++;
+	}
+	if (st[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*left_line(char *storage)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
+		i++;
+	if (!storage[i] || !storage[i + 1])
+	{
+		free(storage);
+		return (NULL);
+	}
+	temp = ft_substr(storage, i + 1, ft_strlen(storage));
+	free(storage);
+	return (temp);
+}
+
+// int	main(void)
 // {
-//     static char *storage;
-//     char        *line;
-//     char        *buffer;
-// 	if (fd < 0 || (BUFFER_SIZE <= 0))
-//         return NULL;
-//     else if
-//         line = get_line(fd, storage, buffer, BUFFER_SIZE);
-//     free_storage(storage);  
-//     return (line);
+// 	int fd;
+// 	char *line;
+// 	fd = open("archivo.txt", O_RDONLY);
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
 // }
