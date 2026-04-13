c2ff8525582fac68b710426809aa5eb3dcb3e48e
betferna <betferna@student.codam.nl>
2026-04-13 15:46:17 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: betferna <betferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 11:45:04 by betferna          #+#    #+#             */
-/*   Updated: 2026/04/09 15:29:53 by betferna         ###   ########.fr       */
+/*   Updated: 2026/04/13 15:44:40 by betferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,6 @@ char	*free_storage(char *storage)
 		storage = NULL;
 	}
 	return (NULL);
-}
-
-char	*ft_read(int fd, char *storage, char *buffer)
-{
-	int		bytes;
-	char	*temp;
-
-	bytes = 0;
-	while (!newline(storage))
-	{
-		bytes = read(fd, buffer, BUFFER_SIZE);
-		if (bytes == 0)
-			break ;
-		else if (bytes == -1 || !storage)
-			storage = free_storage(storage);
-		buffer[bytes] = '\0';
-		temp = storage;
-		storage = ft_strjoin(temp, buffer);
-		free(temp);
-	}
-	free(buffer);
-	if (!storage || *storage == '\0')
-		storage = free_storage(storage);
-	return (storage);
 }
 
 char	*find_newline(char *st)
@@ -88,6 +64,34 @@ char	*left_line(char *storage)
 	return (temp);
 }
 
+char	*ft_read(int fd, char *storage, char *buffer)
+{
+	int		bytes;
+	char	*temp;
+
+	bytes = 0;
+	while (!newline(storage))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == 0)
+			break ;
+		else if (bytes == -1 || !storage)
+		{
+			storage = free_storage(storage);
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		temp = storage;
+		storage = ft_strjoin(temp, buffer);
+		free(temp);
+	}
+	free(buffer);
+	if (!storage || *storage == '\0')
+		storage = free_storage(storage);
+	return (storage);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*storage;
@@ -108,3 +112,16 @@ char	*get_next_line(int fd)
 	storage = left_line(storage);
 	return (line);
 }
+
+// int main()
+// {
+// 	char *line;
+// 	int fd = open("file.txt", O_RDWR | O_CREAT);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = NULL;
+// 	close(fd);
+// 	// remove("file.txt");
+// 	return(0);
+// }
